1b35d725714e2d9039a2ff2d24e74e5de29e1f4b
Bart <bart.vanderwal@han.nl>
2022-10-19 13:11:07 +0200

    Iss #2: Chrome extension edit page.

---
+++ background.js
@@ -0,0 +1,45 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.action.setBadgeText({
+        text: "OFF",
+    })
+})
+
+chrome.action.onClicked.addListener(async (tab) => {
+    const urlPrefix = 'https://isas.han.nl'
+    const localhost = 'http://localhost'
+    console.log("tab.url: ", tab.url)
+
+    if (tab.url.startsWith(urlPrefix) || tab.url.startsWith(localhost)) {
+      // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
+      const prevState = await chrome.action.getBadgeText({ tabId: tab.id })
+      // Next state will always be the opposite
+      const nextState = prevState === 'ON' ? 'OFF' : 'ON'
+  
+      // Set the action badge to the next state
+      await chrome.action.setBadgeText({
+        tabId: tab.id,
+        text: nextState,
+      })
+
+      scripting.executeScript( {
+        target: {tabId: tab.id},
+        files: ['popup.js'],
+      },() => { console.log('script executed (?)') }) 
+
+      if (nextState === "ON") {
+        // Insert the CSS file when the user turns the extension on
+        await chrome.scripting.insertCSS({
+          files: ["css/style.css"],
+          target: { tabId: tab.id },
+        })
+      } else if (nextState === "OFF") {
+        // Remove the CSS file when the user turns the extension off
+        await chrome.scripting.removeCSS({
+          files: ["css/style.css"],
+          target: { tabId: tab.id },
+        })
+      }
+    } else {
+        alert('De iSAS pick random student plugin werkt alleen op url ' + urlPrefix)
+    }
+  })
--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-alert('pick a student');
+alert('pick a student')
 
-console.log('test console');
-document.querySelectorAll('.ui-li-heading span').forEach(i => console.log(i.textContent));
+console.log('test console')
+document.querySelectorAll('.ui-li-heading span').forEach(i => console.log(i.textContent))
