7a00ea49caa3cc3138a86baaf9624415ca18b07c
Bart <bart.vanderwal@han.nl>
2022-10-30 15:49:35 +0100

    Docent antwoord werkt issue #7 punt 2.

--- backend/public/app.ts
+++ backend/public/app.ts
@@ -1,5 +1,6 @@
 import { Vraag } from "../shared/models/vraag";
 import { Antwoord } from "../shared/models/antwoord"
+import { HtmlElement } from "../../../../node_modules/html-validate/build/shim";
 
 const requiredMessage = "Dit veld is verplicht!"
 
@@ -8,15 +9,16 @@ const requiredMessage = "Dit veld is verplicht!"
  * @param {Object} options
  * @param {string} options.url - URL to POST data to
  * @param {FormData} options.formData - `FormData` instance
+ * @param method Http/REST method to use to send data: GET, POST, DELETE etc. (default: POST)
  * @return {Object} - Response body from URL that was POSTed to
  * Bron: https://simonplend.com/how-to-use-fetch-to-post-form-data-as-json-to-your-api/
  */
- async function postFormDataAsJson({ url, formData }) {
+ async function postFormDataAsJson({ url, formData }, method = 'POST') {
 	const plainFormData = Object.fromEntries(formData.entries());
 	const formDataJsonString = JSON.stringify(plainFormData);
 
 	const fetchOptions = {
-		method: "POST",
+		method: method,
 		headers: {
 			"Content-Type": "application/json",
 			Accept: "application/json",
@@ -40,75 +42,68 @@ const requiredMessage = "Dit veld is verplicht!"
  * @param {SubmitEvent} event
  * Bron: https://simonplend.com/how-to-use-fetch-to-post-form-data-as-json-to-your-api/
  */
- async function handleFormSubmit(event) {
+ async function handleFormSubmit<T>(event) {
 	console.log('handleFormSubmit')
 	event.preventDefault()
 
 	const form = event.currentTarget
 	const url = form.action
 
-	try {
-		const formData = new FormData(form);
-		const responseData = await postFormDataAsJson({ url, formData })
-		console.log({ responseData })
-		return responseData
-	} catch (error) {
-		console.error(error)
+	const formData = new FormData(form);
+	const responseData = await postFormDataAsJson({ url, formData })
+	console.log({ responseData })
+	const response = responseData as T
+	if (response==null) {
+		throw new Error(`Response is niet van verwachte type.`)
 	}
+	return responseData as T
 }
 
 const vraagForm = document.getElementById("vraag-form")
 vraagForm.addEventListener("submit", async (event) => {
 	
-	const result = await handleFormSubmit(event);
-	voegVraagToe({id: result.id, vraagTekst: result.vraagTekst})
+	const result = await handleFormSubmit<Vraag>(event)
+	voegVraagToe(result)
 
 	// Wissen vraag uit input veld na toevoegen
 	let vraag = <HTMLInputElement> document.getElementById('vraagTekst')
-	vraag.value = '';
+	vraag.value = ''
+})
+
+const antwoordForm = document.getElementById("antwoord-form")
+antwoordForm.addEventListener("submit", async (event) => {
+	const result = await handleFormSubmit<Antwoord>(event)
+	voegAntwoordToe(result)
 })
 
 let huidigeVraagId = ''
 let vragen: Array<Vraag> = []
 let antwoorden: Array<Antwoord> = []
 
-// Bronnen: 
-// - https://stackoverflow.com/questions/16434773/raw-javascript-equivalent-to-jquery-empty#answer-16435191
-// - https://stackoverflow.com/questions/51679889/extend-htmlelement-prototype
-class ClearableHTMLElement extends HTMLElement {
-	clear(el: ClearableHTMLElement) {
-		while (this.lastChild) {
-			this.removeChild(this.lastChild);
-		}
-	}
-}
-
-declare global {
-	interface HTMLElement {
-    	clearChildren(el: ClearableHTMLElement): void
-	}
-}
-ClearableHTMLElement.prototype.clearChildren = (el: ClearableHTMLElement) => {
-	const element = el // this as Element
+/**
+ * Maak element leeg
+ * Bron: https://stackoverflow.com/questions/16434773/raw-javascript-equivalent-to-jquery-empty#answer-16435191
+ */
+function clearChildren(element: HtmlElement) {
 	while (element.lastChild) {
 		element.removeChild(element.lastChild);
 	}
 }
 
 function haalAntwoordenOp(vraagId: string) {
-	const antwoordenKop = document.getElementById('antwoordenKop') as ClearableHTMLElement;
-	const antwoordenLijst = document.getElementById('antwoorden')
-	document.getElementById('antwoordenLoading').innerText = 'Ophalen...'
+	const antwoordenKop = document.getElementById('antwoordenKop')
+	const antwoordenStatus = document.getElementById('antwoordenStatus')
+	antwoordenStatus.innerText = 'Ophalen...'
 
 	fetch(`/api/antwoorden/${vraagId}`)
 	.then(response => response.json())
 	.then(data => { console.log(`Antwoorden vraag '${vraagId}': `, data); return data} )
 	.then(data => {
 		antwoorden = data.antwoorden
-		antwoordenLijst.clearChildren(antwoordenKop)
-		document.getElementById('antwoordenLoading').setAttribute('style', 'display: none')
+		clearChildren(antwoordenLijst)
+		antwoordenStatus.innerText = ''
 		if (!antwoorden || antwoorden.length<1) {
-			antwoordenKop.insertAdjacentHTML("afterend", "<p>Nog geen vragen.</p>")
+			antwoordenStatus.innerText = "Er zijn nog geen antwoorden voor deze vraag"
 		} else {
 			antwoorden.forEach(a => {
 				voegAntwoordToe(a)			
@@ -117,7 +112,8 @@ function haalAntwoordenOp(vraagId: string) {
 	})
 }
 
-const vragenLijst = document.getElementById('vragen');
+const vragenLijst = document.getElementById('vragen')x
+const antwoordenLijst = document.getElementById('antwoorden')
 
 function voegVraagToe(v: Vraag) {
 	const li = document.createElement('li')
@@ -125,15 +121,16 @@ function voegVraagToe(v: Vraag) {
 	li.setAttribute('id', v.id)
 	vragenLijst.appendChild(li)
 	document.getElementById(v.id).insertAdjacentHTML("afterbegin", '<i class="fa fa-trash fa-small" aria-hidden="true">&nbsp;')
+
 	// Maak elke vraag selecteerbaar door op het list item te klikken te klikken.
-	maakVraagSelecteerbaar(li, v.id);
+	maakVraagSelecteerbaar(li, v);
 }
 
 function voegAntwoordToe(a: Antwoord) {
 	const li = document.createElement('li')
 	li.textContent = a.antwoordTekst
 	li.setAttribute('id', a.id)
-	vragenLijst.appendChild(li)
+	antwoordenLijst.appendChild(li)
 	document.getElementById(a.id).insertAdjacentHTML("afterbegin",
 		'<i class="fa fa-check fa-small" aria-hidden="true"></i>&nbsp;' +
 		'<i class="fa-exclamation-triangle fa-small" aria-hidden="true"></i>&nbsp;' +
@@ -141,32 +138,40 @@ function voegAntwoordToe(a: Antwoord) {
 }
 
 
-function maakVraagSelecteerbaar(li: Element, vraagId) {
+function maakVraagSelecteerbaar(li: Element, vraag: Vraag) {
 	li.addEventListener('click', event => {
-		const elActive = event.target as Element
-		selecteerAlsHuidigeVraag(elActive, vraagId)
+		selecteerAlsHuidigeVraag(vraag)
 	});
 }
 
-function selecteerAlsHuidigeVraag(el: Element, vraagId: string) {
-	el.setAttribute('class', 'active');
-	let oudeVraagLi = document.getElementById(huidigeVraagId);
-	oudeVraagLi.classList.remove('active')
-	huidigeVraagId = vraagId
+function selecteerAlsHuidigeVraag(vraag: Vraag) {
+	// Markeer nieuwe vraag.
+	const vraagId = vraag.id
+	document.getElementById(vraagId).setAttribute('class', 'active');
+
+	// Haal markering van vorige vraag af (mits anders)
+	if (vraag.id!==huidigeVraagId) {
+		let oudeVraagLi = document.getElementById(huidigeVraagId);
+		oudeVraagLi.classList.remove('active')
+	}
+	huidigeVraagId = vraagId;
+	(<HTMLInputElement> document.getElementById('huidigeVraagId')).value = vraagId;
+	(<HTMLInputElement> document.getElementById('huidigeVraagTekst')).value = vraag.vraagTekst;
 	haalAntwoordenOp(vraagId)
 }
 
 // TODO: UI Uitbreiden met lessen, en alleen vragen voor huidige les laten zien.
 function haalVragenOp() {
 	const vragenKop = document.getElementById('vragenKop');
-	vragenKop.insertAdjacentHTML("afterend", "<p id='vragenLoading'>Ophalen...</p>")
+	const vragenStatus = document.getElementById('vragenStatus')
+	vragenStatus.innerText = 'Ophalen...'
 
 	fetch('/api/vragen')
 	.then(response => response.json())
 	.then(data => { console.log(data); return data} )
 	.then(data => {
 		vragen = data.vragen;
-		document.getElementById('vragenLoading').setAttribute('style', 'display: none')
+		vragenStatus.innerText = ''
 		if (vragen && vragen.length>=1) {
 			huidigeVraagId = vragen[0].id
 		} else {
